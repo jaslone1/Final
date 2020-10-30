20286cfaec693cfc07bc599fc9d3e93abb1458f7
jaslone <jaredmslone@gmail.com>
2020-10-30 11:55:38 -0500

    new update fields

--- public/form.js
+++ public/form.js
@@ -19,7 +19,8 @@ class Form extends React.Component {
             '/api/bikes',
             {
                 brand:this.state.newBikeBrand,
-                models:this.state.newBikeModel,
+                model:this.state.newBikeModel,
+                maintenance:this.state.newBikeMaintenance,
             }
         ).then(
             (response) => {
@@ -41,6 +42,11 @@ class Form extends React.Component {
             newBikeBrand:event.target.value
         });
     }
+    changeNewBikeMaintenance = (event) => {
+        this.setState({
+            newBikeMaintenance:event.target.value
+        });
+    }
 
     deleteBike = (event) => {
         axios.delete('/api/bikes/' + event.target.value).then(
@@ -60,14 +66,16 @@ class Form extends React.Component {
             '/api/bikes/' + id,
             {
                 brand:this.state.updateBikeBrand,
-                models:this.state.updateBikeModel,
+                model:this.state.updateBikeModel,
+                maintenance:this.state.updateBikeMaintenance,
             }
         ).then(
             (response) => {
                 this.setState({
                     bikes:response.data,
                     brand:'',
-                    models:'',
+                    model:'',
+                    maintenance:'',
                 })
             }
         )
@@ -88,13 +96,21 @@ class Form extends React.Component {
             }
         )
     }
+    changeUpdateBikeMaintenance = (event) => {
+        this.setState(
+            {
+                updateBikeMaintenance:event.target.value
+            }
+        )
+    }
 
     render = () => {
         return <div>
-            <h2>Create Bike</h2>
+            <h2>Add a new Bike</h2>
             <form onSubmit={this.createBike}>
                 <input onKeyUp={this.changeNewBikeBrand} type="text" placeholder="brand" /><br/>
                 <input onKeyUp={this.changeNewBikeModel} type="text" placeholder="models" /><br/>
+                <input onKeyUp={this.changeNewBikeMaintenance} type="textarea" placeholder="models" /><br/>
                 <input type="submit" value="Create Bike" />
             </form>
             <h2>List of Bikes</h2>
@@ -104,13 +120,14 @@ class Form extends React.Component {
                         (bike, index) => {
                             return <li key={index}>
 
-                                {bike.brand}: {bike.models}
+                                {bike.brand}: {bike.model}
 
                                 <button value={bike.id} onClick={this.deleteBike}>DELETE</button>
 
                                 <form id={bike.id} onSubmit={this.updateBike}>
                                     <input onKeyUp={this.changeUpdateBikeBrand} type="text" placeholder="brand"/><br/>
                                     <input onKeyUp={this.changeUpdateBikeModel} type="text" placeholder="models"/><br/>
+                                      <input onKeyUp={this.changeUpdateBikeMaintenance} type="text" placeholder="maintenance"/><br/>
                                     <input type="submit" value="Update Bike"/>
                                 </form>
                             </li>
